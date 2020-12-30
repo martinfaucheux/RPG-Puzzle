964bf4e5e9f04dd40407327b90895a380b4ef812
martinfaucheux <martin.faucheux@gmail.com>
2020-12-30 18:59:53 +0700

    upgrade to unity 2020.2.1f1

--- Assets/Scripts/AutoSprite/AutoRandomSprite.cs
+++ Assets/Scripts/AutoSprite/AutoRandomSprite.cs
@@ -31,9 +31,6 @@ public class AutoRandomSprite : MonoBehaviour
         if (spriteCount == 0)
             return null;
 
-        // Create a Random object  
-        Random rand = new Random();
-
         // Generate a random index less than the size of the array.  
         int index = Random.Range(0, spriteCount);
 
