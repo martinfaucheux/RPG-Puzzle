b434146e604a2910b7584f3834dba2ad2a4fb892
martinfaucheux <martin.faucheux@gmail.com>
2021-04-27 19:27:48 +0700

    add earth crack component

---
+++ Assets/Scripts/EntityComponents/Crack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crack : ActivableObject
+{
+    private bool isWalkable = true;
+
+    public Sprite activatedSprite;
+
+    // return true if source object can move after activation
+    public override bool Activate(GameObject sourceObject = null)
+    {
+        if(isWalkable){
+            isWalkable = false;
+            UpdateSprite();
+            return true;
+        }
+        return false;
+    }
+
+    private void UpdateSprite(){
+        SpriteRenderer spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        spriteRenderer.sprite = activatedSprite;
+    }
+}
