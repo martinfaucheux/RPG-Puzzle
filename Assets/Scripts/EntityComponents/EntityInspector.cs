1d952d99de5401b9a995f9775c560800b55c8995
martinfaucheux <martin.faucheux@gmail.com>
2021-05-04 19:13:37 +0700

    disable inspector click

--- Assets/Scripts/EntityComponents/EntityInspector.cs
+++ Assets/Scripts/EntityComponents/EntityInspector.cs
@@ -10,21 +10,19 @@ public class EntityInspector : MonoBehaviour
     private Attack _attackComponent;
     private Health _healthComponent;
 
-    // Start is called before the first frame update
     void Start()
     {
         _attackComponent = GetComponent<Attack>();
         _healthComponent = GetComponent<Health>();
     }
 
-    // Update is called once per frame
-    void OnMouseOver()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            MenuController.instance.AttachObject(gameObject);
-            MenuController.instance.Trigger();
-        }
-        UIManager.instance.UpdateUI();
-    }
+    // void OnMouseOver()
+    // {
+    //     if (Input.GetMouseButtonDown(0))
+    //     {
+    //         MenuController.instance.AttachObject(gameObject);
+    //         MenuController.instance.Trigger();
+    //     }
+    //     UIManager.instance.UpdateUI();
+    // }
 }
