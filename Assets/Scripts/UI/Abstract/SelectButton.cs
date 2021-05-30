369819864a289cd5661818efbfc5275334956d30
martinfaucheux <martin.faucheux@gmail.com>
2021-05-30 21:31:51 +0700

    improve skill button

---
+++ Assets/Scripts/UI/Abstract/SelectButton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class SelectButton : MonoBehaviour, IPointerClickHandler // required interface for OnSelect
+{
+    private static List<SelectButton> _selectButtons;
+    public static SelectButton currentSelected{
+        get; private set;
+    }
+
+    public bool selected{ get; private set;}
+
+    void Awake(){
+        if (_selectButtons == null){
+            _selectButtons = new List<SelectButton>{this};
+        }
+        else{
+            _selectButtons.Add(this);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData) => HandleEvent();
+
+    private void HandleEvent(){
+        UnselectAll();
+        
+        if (!selected){
+            selected = true;
+            currentSelected = this;
+            OnButtonSelect();
+        }
+    }
+
+    protected virtual void OnButtonSelect(){}
+    protected virtual void OnButtonUnselect(){}
+
+    private void UnselectAll(bool ignoreThis = true){
+        foreach(SelectButton button in _selectButtons){
+            if (button.selected && !(button == this && !ignoreThis)){
+                button.OnButtonUnselect();
+                button.selected = false;
+            }
+        }
+    }
+}
--- Assets/Scripts/UI/SkillUI/SkillSlotUI.cs
+++ Assets/Scripts/UI/SkillUI/SkillSlotUI.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SkillSlotUI : MonoBehaviour
+public class SkillSlotUI : SelectButton
 {
     // TODO: This should be moved to SkillManager instead
     public Skill skill;
     public SkillSelectorUI skillSelectorUI{private get; set;}
-    [SerializeField] private Image _imageComponent;
-    [SerializeField] bool _disableButtonOnUnlock;
+    [SerializeField] Image _skillIconImage;
+    [SerializeField] Image _highlightImage;
+    [SerializeField] Image _borderImage;
+    [SerializeField] float stateTransitionDuration = 0.5f;
 
-    private Button _buttonComponent;
 
     void Start()
     {
@@ -19,28 +20,42 @@ public class SkillSlotUI : MonoBehaviour
             SkillManager.instance.RegisterSkill(skill);
             SetSprite();
         }
-
-        _buttonComponent = GetComponent<Button>();
-
         GameEvents.instance.onSkillEnabled += OnUnlockSkill;
-        
     }
 
     public void SetSprite(){
-        _imageComponent.sprite = skill.sprite;
-        Color newColor = _imageComponent.color;
+        _skillIconImage.sprite = skill.sprite;
+        Color newColor = _skillIconImage.color;
         newColor.a = 1;
-        _imageComponent.color = newColor;
+        _skillIconImage.color = newColor;
     }
 
-    public void SelectSkill(){
+    protected override void OnButtonSelect(){
         skillSelectorUI?.SelectSkill(skill);
+        ScaleHighlightImage(1.1f);
+    }
+    protected override void OnButtonUnselect(){
+        ScaleHighlightImage(1f);
     }
 
     private void OnUnlockSkill(Skill skill){
-        if (skill == this.skill && _disableButtonOnUnlock){
-            _buttonComponent.interactable = false;
+        if (skill == this.skill){
+            FadeOutButton();
         }
+    }
+
+    private void FadeOutButton(){
+        // Called when the skill is already unlocked
+        float targetAlphaValue = 0.5f;
+        LeanTween.alpha((RectTransform) _skillIconImage.transform, targetAlphaValue, stateTransitionDuration).setEaseOutQuint();
+        LeanTween.alpha((RectTransform) _borderImage.transform, targetAlphaValue, stateTransitionDuration).setEaseOutQuint();
+        LeanTween.alpha((RectTransform) _highlightImage.transform, 0f, stateTransitionDuration).setEaseOutQuint();
+    }
+
+
+    private void ScaleHighlightImage(float newScale){
+        Vector3 _newScale = newScale * new Vector3(1, 1, 0);
+        LeanTween.scale(_highlightImage.gameObject, _newScale, stateTransitionDuration);
     }
 
     void OnDestroy(){
