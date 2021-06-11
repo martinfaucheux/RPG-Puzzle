8d13fcb22927d9dd9a34086f1a2168cb1c0f73dd
martinfaucheux <martin.faucheux@gmail.com>
2021-06-11 19:47:29 +0700

    change name of scripts

--- Assets/Scripts/UI/Other/GameOverText.cs
+++ Assets/Scripts/UI/Other/GameOverText.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class PressRText : MonoBehaviour
+public class GameOverText : MonoBehaviour
 {
 
     public float textFadePeriod = 2f;
