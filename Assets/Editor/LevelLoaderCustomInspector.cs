9f7bd56c5cfc52c67f9f4ca150b25c3411142ab6
Martin Faucheux <martin.faucheux@gmail.com>
2021-12-16 10:45:13 +0700

    add unlock all button

--- Assets/Editor/LevelLoaderCustomInspector.cs
+++ Assets/Editor/LevelLoaderCustomInspector.cs
@@ -1,57 +1,50 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using UnityEditor.SceneManagement;
+using System.Collections.Generic;
+using System.Linq;
 
 [CustomEditor(typeof(LevelLoader))]
 public class LevelLoaderCustomInspector : Editor
 {
     private LevelLoader t;
 
-    private void OnEnable() {
-        t = target as LevelLoader;
-        SceneView.duringSceneGui -= OnScene;
-        SceneView.duringSceneGui += OnScene;
-    }
-
-    void OnScene(SceneView sceneview)
+    private void OnEnable()
     {
-        // if (t.showSceneBounds)
-        // {
-        //     Vector2 origin = t.origin - Vector2.one * 0.5f;
-        //     float matrixWidth = t.matrixSize.x;
-        //     float matrixHeight = t.matrixSize.y;
-
-        //     Vector3[] verts = new Vector3[]
-        //     {
-        //     new Vector3(origin.x, origin.y),
-        //     new Vector3(origin.x, origin.y + matrixHeight),
-        //     new Vector3(origin.x + matrixWidth, origin.y + matrixHeight),
-        //     new Vector3(origin.x + matrixWidth, origin.y),
-        //     };
-
-        //     Handles.DrawSolidRectangleWithOutline(verts, t.sceneBoundsColor, new Color(0, 0, 0, 1));
-        // }
+        t = target as LevelLoader;
     }
+
+
 
     public override void OnInspectorGUI()
     {
         // Show default inspector property editor
         DrawDefaultInspector();
 
-        // if (GUILayout.Button("Load Saved State"))
-        // {
-        //     t.LoadLastLevelPlayed();
-        // }
 
         if (GUILayout.Button("Delete Saved State"))
         {
             t.DeleteSavedData();
         }
 
-        // if (GUI.changed)
-        // {
-        //     EditorUtility.SetDirty(t);
-        //     EditorSceneManager.MarkSceneDirty(t.gameObject.scene);
-        // }
+        if (GUILayout.Button("Unlock all levels"))
+        {
+            UnlockAllLevels();
+        }
+
+
+    }
+
+    private void UnlockAllLevels()
+    {
+        Dictionary<int, LevelSaveData> levelDict = new Dictionary<int, LevelSaveData>();
+        foreach (LevelMetaData levelMetaData in t.levelCollection.levelList)
+        {
+            bool[] gemsCollected = new List<bool>(levelMetaData.gemCount).ToArray();
+            bool[] questsCompleted = new bool[] { false };
+            levelDict[levelMetaData.sceneBuildIndex] = new LevelSaveData(gemsCollected, questsCompleted);
+        }
+
+        PlayerData playerSavedData = new PlayerData(levelDict);
+        DataSaver.SaveGameState(playerSavedData);
     }
 }
