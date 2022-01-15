b3ef48e8d9f350deafc1dce2232cc3e34009acd2
Martin Faucheux <martin.faucheux@gmail.com>
2022-01-15 11:48:20 +0100

    add level up text

--- Assets/Scripts/EntityComponents/LevelUpAnimHandler.cs
+++ Assets/Scripts/EntityComponents/LevelUpAnimHandler.cs
@@ -1,25 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LevelUpAnimHandler : MonoBehaviour
 {
-    public SpriteFlasher spriteFlasher;
-    public Animator animator;
-    
+    [SerializeField] SpriteFlasher spriteFlasher;
+    [SerializeField] Animator animator;
+    [SerializeField] TextMeshPro floatingTextComponent;
+    [SerializeField] float textAnimationDuration = 1.5f;
+
+    private Vector3 initFloatingTextPos;
 
     void Start()
     {
         GameEvents.instance.onEnterLevelUp += AnimateLevelUp;
+        initFloatingTextPos = floatingTextComponent.transform.localPosition;
     }
-    
-    void Update(){
+
+    void Update()
+    {
+        // TODO: This is for debugging. Remove this.
         if (Input.GetKeyDown(KeyCode.P))
             AnimateLevelUp();
     }
 
-    private void AnimateLevelUp(){
+    public void AnimateLevelUp()
+    {
         spriteFlasher.Flash();
         animator.SetTrigger("levelup");
+
+        AnimateFloatingText();
+    }
+
+    private void AnimateFloatingText()
+    {
+        Transform t = floatingTextComponent.transform;
+        t.localPosition = initFloatingTextPos;
+        Vector3 targetPos = t.position + 2f * Vector3.up;
+
+        floatingTextComponent.gameObject.SetActive(true);
+        LeanTween.move(t.gameObject, targetPos, textAnimationDuration).setOnComplete(DisableFloatingText);
+
+        floatingTextComponent.alpha = 1f;
+        LeanTween.value(floatingTextComponent.gameObject, tmProAlphaCallback, 1f, 0f, textAnimationDuration).setEaseInCubic();
+    }
+
+
+    void tmProAlphaCallback(float alpha)
+    {
+        Color newColor = floatingTextComponent.color;
+        newColor.a = alpha;
+        floatingTextComponent.color = newColor;
+    }
+
+
+
+
+    private void DisableFloatingText()
+    {
+        floatingTextComponent.gameObject.SetActive(false);
     }
 }
