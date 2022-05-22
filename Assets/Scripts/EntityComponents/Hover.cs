55e33e11be2d621e9ed1c2098a241f1390c10dfd
Martin Faucheux <martin.faucheux@gmail.com>
2022-05-22 11:50:27 +0200

    randomize spriteholder hover

--- Assets/Scripts/EntityComponents/Hover.cs
+++ Assets/Scripts/EntityComponents/Hover.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class Hover : MonoBehaviour
 {
-
     public bool hoverEnabled = false;
-
     public float hoverAmplitude = 0.1f;
-    public float hoverPerdiod = 6f; 
+    public float hoverPerdiod = 6f;
+    public bool randomize = true;
 
     private Vector3 _initPos;
-    private void Start(){
+    private float _randomness = 0f;
+
+    private void Start()
+    {
         _initPos = transform.position;
+        if (randomize)
+        {
+            _randomness = Random.Range(0f, hoverPerdiod);
+            Debug.Log("randomness: " + _randomness.ToString());
+        }
     }
 
     private void Update()
     {
         if (hoverEnabled)
         {
-            float posDifference = hoverAmplitude * Mathf.Sin(2 * Mathf.PI * Time.time / hoverPerdiod);
+            float posDifference = hoverAmplitude * Mathf.Sin(2 * Mathf.PI * (Time.time + _randomness) / hoverPerdiod);
             Vector3 newPos = _initPos + posDifference * Vector3.up;
             transform.position = newPos;
         }
